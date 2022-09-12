bb1563fe747aed3aa9ffc0937059eca0b915fbd7
dxthanh2002 <109643053+dxthanh2002@users.noreply.github.com>
2022-09-12 19:00:55 +0700

    up

--- js/bt6.js
+++ js/bt6.js
@@ -15,7 +15,7 @@ function forecastAjax(){
         }
 
     }
-    var link="http://api.openweathermap.org/data/2.5/forecast?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
+    var link="https://api.openweathermap.org/data/2.5/forecast?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
     xhttp.open("get",link,true);
     xhttp.send();
 }
