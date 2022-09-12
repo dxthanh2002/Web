4e6938a758302bcb001beeeff0078ca33f48ab77
dxthanh2002 <109643053+dxthanh2002@users.noreply.github.com>
2022-09-12 18:59:09 +0700

    up

---
+++ js/bt6.js
@@ -0,0 +1,22 @@
+function forecastAjax(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function (){
+        var data = JSON.parse(this.responseText);
+        //....
+        var list = data.list;
+        for( var i = 0;i<list.length;i++){
+            var f = `<div class="box-weather">
+                <h4>${list[i].dt_txt}</h4>
+                <h2>${list[i].main.temp}<sup>o</sup></h2>
+                <p>${list[i].weather[0].description}</p>
+                <img src="https://openweathermap.org/img/wn/10d@2x.png" alt=""/>
+            </div>`;
+            document.getElementById("forecast").innerHTML +=f;
+        }
+
+    }
+    var link="http://api.openweathermap.org/data/2.5/forecast?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
+    xhttp.open("get",link,true);
+    xhttp.send();
+}
+forecastAjax();
